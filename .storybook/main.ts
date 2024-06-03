b87485975c1681f8427b7f0d70f275f02912c3b4
王首人 <wangshouren@wangshourendeMacBook-Air.local>
2024-06-03 17:27:12 +0800

    feat: add env in storybook in vercel

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,11 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import { DefinePlugin } from "webpack";
+import { merge } from "webpack-merge";
 
-console.log("storybook", process.env.NODE_ENV);
+console.log(
+  "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY",
+  process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+);
 
 const config: StorybookConfig = {
   stories: [
@@ -17,6 +22,31 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/nextjs",
     options: {},
+  },
+  webpackFinal(config) {
+    // copy env if it is built in vercel
+    if (!process.env.VERCEL) {
+      return config;
+    }
+
+    return merge(config, {
+      plugins: [
+        new DefinePlugin({
+          "process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY": JSON.stringify(
+            process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+          ),
+          "process.env.CLERK_SECRET_KEY": JSON.stringify(
+            process.env.CLERK_SECRET_KEY
+          ),
+          "process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL": JSON.stringify(
+            process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL
+          ),
+          "process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL": JSON.stringify(
+            process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL
+          ),
+        }),
+      ],
+    });
   },
   staticDirs: process.env.NODE_ENV === "development" ? ["../public"] : [], // in build mode, it is built into public folder, so we didn't need to specify it.
 };
