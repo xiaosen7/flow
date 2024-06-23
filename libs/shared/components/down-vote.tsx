1a76a55c87b2b9122f5882186db925b60f36eb47
王首人 <wangshouren@wangshourendeMacBook-Air.local>
2024-06-23 10:17:17 +0800

    feat: toast for votes

--- libs/shared/components/down-vote.tsx
+++ libs/shared/components/down-vote.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { IComponentBaseProps, formatNumber, mp } from "@/shared";
+import { IComponentBaseProps, formatNumber, mp, toast } from "@/shared";
 import Image from "next/image";
 import React from "react";
 import { imageDownvoteSrc } from "../assets/icons/downvote";
@@ -8,7 +8,7 @@ import { imageDownvotedSrc } from "../assets/icons/downvoted";
 export interface IDownVoteProps extends IComponentBaseProps {
   count: number;
   voted?: boolean;
-  onChange?: (vote: boolean) => void;
+  onChange?: (vote: boolean) => void | Promise<void>;
 }
 
 export const DownVote: React.FC<IDownVoteProps> = (props) => {
@@ -22,7 +22,14 @@ export const DownVote: React.FC<IDownVoteProps> = (props) => {
         height={18}
         src={voted ? imageDownvotedSrc : imageDownvoteSrc}
         width={18}
-        onClick={() => onChange?.(!voted)}
+        onClick={() =>
+          onChange &&
+          Promise.resolve(onChange(!voted)).then(() => {
+            toast({
+              title: `Down vote ${voted ? "removed" : "added"}`,
+            });
+          })
+        }
       />
 
       <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
--- libs/shared/components/up-vote.tsx
+++ libs/shared/components/up-vote.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { IComponentBaseProps, formatNumber, mp } from "@/shared";
+import { IComponentBaseProps, formatNumber, mp, toast } from "@/shared";
 import Image from "next/image";
 import React from "react";
 import { imageUpvoteSrc } from "../assets/icons/upvote";
@@ -8,7 +8,7 @@ import { imageUpvotedSrc } from "../assets/icons/upvoted";
 export interface IUpVoteProps extends IComponentBaseProps {
   count: number;
   voted?: boolean;
-  onChange?: (vote: boolean) => void;
+  onChange?: (vote: boolean) => void | Promise<void>;
 }
 
 export const UpVote: React.FC<IUpVoteProps> = (props) => {
@@ -22,7 +22,14 @@ export const UpVote: React.FC<IUpVoteProps> = (props) => {
         height={18}
         src={voted ? imageUpvotedSrc : imageUpvoteSrc}
         width={18}
-        onClick={() => onChange?.(!voted)}
+        onClick={() =>
+          onChange &&
+          Promise.resolve(onChange(!voted)).then(() => {
+            toast({
+              title: `Up vote ${voted ? "removed" : "added"}`,
+            });
+          })
+        }
       />
 
       <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
